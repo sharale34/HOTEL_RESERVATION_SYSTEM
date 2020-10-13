67173cd66d60614f8aeb6356d63fe8c54db3f8bb
sharale34 <sourabhharale8000@gmail.com>
2020-10-13 17:28:00 +0530

    Added: Welcome message is added

---
+++ HotelReservation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelreservation;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+
+}
